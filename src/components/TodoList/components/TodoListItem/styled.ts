6c4d7c5f3de9eb20bb5fb15dd0fbb977ab86ef94
Minnia <jasminestockholm@gmail.com>
2020-04-07 20:23:44 +0200

    add initial styling for todos backgrounds

--- src/components/TodoList/components/TodoListItem/styled.ts
+++ src/components/TodoList/components/TodoListItem/styled.ts
@@ -1,1 +1,10 @@
 import styled from 'styled-components';
+import {Text} from 'react-native';
+
+export const EvenTodo = styled(Text)`
+  background-color: blue;
+`;
+
+export const OddTodo = styled(Text)`
+  background-color: grey;
+`;
