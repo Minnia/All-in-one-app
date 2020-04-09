32cbbef544e407b8a75db8c6fea50f5192d07ef2
Minnia <jasminestockholm@gmail.com>
2020-04-09 12:23:05 +0200

    add functionality for todos

--- src/components/TodoList/components/TodoListItem/TodoListItem.tsx
+++ src/components/TodoList/components/TodoListItem/TodoListItem.tsx
@@ -4,20 +4,35 @@ import {TextInput} from 'react-native-gesture-handler';
 import CheckBox from 'react-native-check-box';
 
 interface TodoListItemProps {
-  todo: {
-    text: string;
-    complete: boolean;
-  };
+  text: string;
+  complete: boolean;
 }
 
-const TodoListItem: FunctionComponent<TodoListItemProps> = ({todo}) => {
+const TodoListItem: FunctionComponent<TodoListItemProps> = () => {
   const [value, setValue] = useState<string>('');
   const [todoList, setTodoList] = useState<TodoListItemProps[]>([]);
   const [error, showError] = useState<boolean>(false);
 
   const handleSubmit = (): void => {
     if (value.trim()) {
+      setTodoList([...todoList, {text: value, complete: false}]);
+      setValue('');
+    } else {
+      showError(true);
+      setValue('');
     }
+  };
+
+  const removeItem = (index: number) => {
+    const newTodoList = [...todoList];
+    newTodoList.splice(index, 1);
+    setTodoList(newTodoList);
+  };
+
+  const toggleComplete = (index: number): void => {
+    const newTodoList = [...todoList];
+    newTodoList[index].complete = !newTodoList[index].complete;
+    setTodoList(newTodoList);
   };
 
   return (
@@ -28,32 +43,36 @@ const TodoListItem: FunctionComponent<TodoListItemProps> = ({todo}) => {
           <TextInput
             placeholder="Enter your todo here"
             value={value}
-            onChange={e => {}}
+            onChangeText={e => {
+              setValue(e), showError(false);
+            }}
             style={styles.inputBox}
           />
-          <Button title="Add task" onPress={() => {}} />
+          <Button title="Add task" color="blue" onPress={handleSubmit} />
         </View>
         {error && <Text style={styles.error}>Error: Input field is empty</Text>}
         <Text style={styles.subtitle}>Your tasks: </Text>
         {todoList.length === 0 && <Text>No todos available</Text>}
         {todoList.map((todo: TodoListItemProps, index: number) => (
-          <View style={styles.listItem} key={`${index}_${todo.todo.text}`}>
+          <View style={styles.listItem} key={`${index}_${todo.text}`}>
             <Text
               style={[
                 styles.task,
                 {
-                  textDecorationLine: todo.todo.complete
-                    ? 'line-through'
-                    : 'none',
+                  textDecorationLine: todo.complete ? 'line-through' : 'none',
                 },
               ]}>
-              {todo.todo.text}
+              {todo.text}
             </Text>
             <Button
-              title={todo.todo.complete ? 'Completed' : 'Complete'}
-              onPress={() => {}}
+              title={todo.complete ? 'Completed' : 'Complete'}
+              onPress={() => toggleComplete(index)}
             />
-            <Button title="X" color="crimson" onPress={() => {}} />
+            <Button
+              title="X"
+              color="crimson"
+              onPress={() => removeItem(index)}
+            />
           </View>
         ))}
       </View>
@@ -70,25 +89,28 @@ const styles = StyleSheet.create({
     width: '100%',
     flexDirection: 'row',
     justifyContent: 'space-between',
+    alignItems: 'center',
     marginBottom: 20,
   },
   inputBox: {
     width: 200,
-    borderColor: 'purple',
-    borderRadius: 8,
+    borderBottomColor: 'blue',
+    borderTopColor: 'transparent',
+    borderLeftColor: 'transparent',
+    borderRightColor: 'transparent',
     borderWidth: 2,
     paddingLeft: 8,
+    color: 'black',
   },
   title: {
-    fontSize: 40,
+    fontSize: 30,
     marginBottom: 40,
     fontWeight: 'bold',
-    textDecorationLine: 'underline',
   },
   subtitle: {
     fontSize: 20,
     marginBottom: 20,
-    color: 'purple',
+    color: 'blue',
   },
   listItem: {
     flexDirection: 'row',
